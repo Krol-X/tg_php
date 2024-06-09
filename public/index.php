3e5a42f7d9fa4f8916196ea1959f5a04522c8d17
Alex Krol <krolmail@list.ru>
2024-06-09 16:44:09 +0300

    fix index.php, add token

--- public/index.php
+++ public/index.php
@@ -1,22 +1,34 @@
 <?php
 
 # Принимаем запрос
-$data = json_decode(file_get_contents('php://input'), TRUE);
-//file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
+$input = file_get_contents('php://input');
+$data = json_decode($input, TRUE);
 
+// Логируем входящий запрос для отладки
+file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
 
-//https://api.telegram.org/bot*Токен бота*/setwebhook?url=*ссылка на бота*
+// Проверяем, что данные не пустые и корректные
+if (is_null($data)) {
+    // Если нет данных, можно вернуть HTTP статус 400 или просто завершить скрипт
+    http_response_code(400);
+    echo "No data provided";
+    exit;
+}
 
+// Обрабатываем ручной ввод или нажатие на кнопку
+$data = isset($data['callback_query']) ? $data['callback_query'] : $data['message'];
 
-# Обрабатываем ручной ввод или нажатие на кнопку
-$data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
+if (!isset($data['text']) && !isset($data['data'])) {
+    http_response_code(400);
+    echo "Invalid data";
+    exit;
+}
 
 # Важные константы
-define('TOKEN', '1310530208:AAHAKEpv_W9k3rWcO9G7ZOrJVcKHGEBqo6c');
+define('TOKEN', '7310194104:AAGbJ-u1dQLOrYdg7NarAUyIHQ7rV40b5AI');
 
 # Записываем сообщение пользователя
-$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
-
+$message = mb_strtolower(($data['text'] ?? $data['data'] ?? 'unknown'),'utf-8');
 
 # Обрабатываем сообщение
 switch ($message)
@@ -48,7 +60,6 @@ switch ($message)
         ];
         break;
 
-
     case 'видео':
         $method = 'sendVideo';
         $send_data = [
@@ -78,7 +89,12 @@ switch ($message)
 }
 
 # Добавляем данные пользователя
-$send_data['chat_id'] = $data['chat']['id'];
+if (isset($data['chat']['id'])) {
+    $send_data['chat_id'] = $data['chat']['id'];
+} else {
+    echo "Chat ID is missing";
+    exit;
+}
 
 $res = sendTelegram($method, $send_data);
 
@@ -92,9 +108,12 @@ function sendTelegram($method, $data, $headers = [])
         CURLOPT_URL => 'https://api.telegram.org/bot' . TOKEN . '/' . $method,
         CURLOPT_POSTFIELDS => json_encode($data),
         CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"), $headers)
-    ]);   
-    
+    ]);
+
     $result = curl_exec($curl);
+    if (curl_errno($curl)) {
+        file_put_contents('curl_errors.txt', curl_error($curl) . "\n", FILE_APPEND);
+    }
     curl_close($curl);
     return (json_decode($result, 1) ? json_decode($result, 1) : $result);
 }
